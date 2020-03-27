72cf46cdcaa7cceeaf90ccb9e4a322018c0433ec
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2020-03-27 23:20:37 +0100

    Uses tabs indentation

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,29 +3,29 @@ import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
-    }).compileComponents();
+	TestBed.configureTestingModule({
+		declarations: [
+		AppComponent
+		],
+	}).compileComponents();
   }));
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+	const fixture = TestBed.createComponent(AppComponent);
+	const app = fixture.componentInstance;
+	expect(app).toBeTruthy();
   });
 
   it(`should have as title 'doctor-at-home'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('doctor-at-home');
+	const fixture = TestBed.createComponent(AppComponent);
+	const app = fixture.componentInstance;
+	expect(app.title).toEqual('doctor-at-home');
   });
 
   it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('doctor-at-home app is running!');
+	const fixture = TestBed.createComponent(AppComponent);
+	fixture.detectChanges();
+	const compiled = fixture.nativeElement;
+	expect(compiled.querySelector('.content span').textContent).toContain('doctor-at-home app is running!');
   });
 });
