5ab9278e72bedcd5e0a8144d4258361cc3e52ea5
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2020-03-29 23:38:20 +0200

    Adds misisng fields in patient edition

--- src/app/patient-editor/patient-editor.component.ts
+++ src/app/patient-editor/patient-editor.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Patient } from '../core/patient.model';
+import { Clinician } from '../core/clinician.model';
 import { PatientsService } from '../core/patients.service';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { FormBuilder, FormGroup, FormControl, Validators, ValidatorFn, AbstractControl } from '@angular/forms';
 import * as libphonenumber from 'google-libphonenumber';
+
+import { AuthService } from '../auth.service';
 
 export class PhoneValidator {
 	static validCountryPhone = (): ValidatorFn => {
@@ -45,13 +48,18 @@ export class PatientEditorComponent implements OnInit {
 	public phoneCtrl : FormControl;
 	public languageCtrl : FormControl;
 	private idCtrl : FormControl;
+	public ageCtrl : FormControl;
+	public cityCtrl : FormControl;
+	public preconditionsCtrl : FormControl;
+
 	public patientForm: FormGroup;
 
-
+	public clinician: Clinician;
 
 	constructor(private patientsService: PatientsService,
 				private route: ActivatedRoute,
-				fb: FormBuilder) {
+				fb: FormBuilder,
+				private authService: AuthService) {
 		this.nameCtrl = fb.control('',
 								  [Validators.required]);
 		this.phoneCtrl = fb.control('',[
@@ -63,16 +71,33 @@ export class PatientEditorComponent implements OnInit {
 			Validators.pattern('fr|de|en|it'),
 		]);
 		this.idCtrl = fb.control('');
+		this.ageCtrl = fb.control('',
+								  [Validators.required,
+								   Validators.min(0),
+								   Validators.max(140)]);
+		this.cityCtrl = fb.control('',
+								   [Validators.required]);
+		this.preconditionsCtrl = fb.control('');
+
 		this.patientForm = fb.group({
 			name: this.nameCtrl,
 			phone: this.phoneCtrl,
 			id: this.idCtrl,
 			language: this.languageCtrl,
+			age: this.ageCtrl,
+			city: this.cityCtrl,
+			preconditions: this.preconditionsCtrl,
 		});
 
 	}
 
 	ngOnInit(): void {
+
+		this.authService.currentClinician.subscribe((clinician) => {
+			this.clinician = clinician;
+		});
+
+
 		this.route.paramMap.subscribe( (params:  ParamMap) => {
 			const id = params.get('patientId:');
 			if ( id != null ) {
@@ -82,6 +107,9 @@ export class PatientEditorComponent implements OnInit {
 					this.idCtrl.setValue(patient.ID);
 					this.phoneCtrl.setValue(patient.Phone);
 					this.languageCtrl.setValue(patient.Language);
+					this.ageCtrl.setValue(patient.Age);
+					this.cityCtrl.setValue(patient.City);
+					this.preconditionsCtrl.setValue(patient.Preconditions);
 				});
 			}
 		});
