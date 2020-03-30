7a08011059d842f148ad4d16e4084c01bb8543a8
Alexandre Tuleu <alexandre.tuleu.2005@polytechnique.org>
2020-03-30 16:51:12 +0200

    Fixes bff json parsing

--- fake-backend/health_report.go
+++ fake-backend/health_report.go
@@ -7,8 +7,8 @@ type HealthReport struct {
 	ID            string    `json:"healthReportID"`
 	PatientID     string    `json:"patientID"`
 	HearthBeat    int       `json:"hearthBeat"`
-	Oxygenation   float64   `json:"oxygenation"`
-	Temperature   float64   `json:"temperature"`
-	BreathingRate int       `json:"breathingRate"`
+	Oxygenation   float64   `json:"oxygenation,string"`
+	Temperature   float64   `json:"temperature,string"`
+	BreathingRate int       `json:"breathingRate,string"`
 	Triage        int       `json:"ML_Triage"`
 }
